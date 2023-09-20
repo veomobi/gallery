a9845fb7d61670726716d7d9b021e67dea07bf4d
veomobi <kacperbuczak294@gmail.com>
2023-09-20 17:59:05 +0200

    basic boilerplate

---
+++ js/lib/components.js
@@ -0,0 +1,69 @@
+class Listener {
+    constructor(event, func){
+        this.event = event;
+        this.func = func;
+        // interface
+    }
+}
+
+class Component {
+    constructor(selector = null,listener = null,values = null,styles = null,id){
+        this.htmlElement = document.querySelector(selector);
+        this.selector = selector;
+        this.values = values;
+        this.listener = listener;
+        this.id = id;
+        this.styles = {}; // e.g. style = ["backgroundColor","red"];
+    }
+
+    attachListener(){
+        if (this.listener != null){
+            this.htmlElement.addEventListener(
+                this.listener.event,
+                ()=>{this.listener.func(this)}
+                // pass in context variable to function during component declaration to access the object in the function 
+            )
+        }
+    }
+
+    renderObjectValueOnHTML(key){
+        this.htmlElement.innerHTML = this.values[key];
+    }
+
+    renderIndependentValueOnHTML(value){
+        this.htmlElement.innerHTML = value;
+    }
+
+    overwriteStyles(){
+        if (this.styles != null){
+            Object.keys(this.styles).forEach(
+                style=>{
+                    this.htmlElement.style[style[0]] = style[1];
+                }
+            )
+        }
+    }
+}
+
+class ComponentManager {
+    constructor (components = {}) {
+        this.components = components;
+    }
+
+    add(component){
+        this.components[component.id] = component;
+        return this;
+    }
+
+    get(key){
+        return this.components[key];
+    }
+
+    attachListeners(){
+        Object.keys(this.components).forEach(
+            key=>{
+                this.components[key].attachListener();
+            }
+        )
+    }
+}
